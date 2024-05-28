21371c3084c3816a63aa4cff94437d64dcc87db4
Kailash Choudhary <kailashjat604@gmail.com>
2024-05-28 19:50:45 +0530

    update routes

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -12,13 +12,13 @@ func GetRouter() *http.ServeMux {
 
 	router.HandleFunc("/", handlers.IndexHandler)
 
-	router.HandleFunc("GET /projects", handlers.GetProjects)
+	router.HandleFunc("/projects/all", handlers.GetProjects)
 
-	router.HandleFunc("POST /projects", handlers.UpdateProjects)
+	router.HandleFunc("/projects", handlers.UpdateProjects)
 
-	router.HandleFunc("GET /github/login", auth.HandleGitHubLogin)
+	router.HandleFunc("/github/login", auth.HandleGitHubLogin)
 
-	router.HandleFunc("GET /github/callback", auth.HandleGitHubCallback)
+	router.HandleFunc("/github/callback", auth.HandleGitHubCallback)
 
 	return router
 }
