5a8659797b699a4520c1f6e0a7267784c3e1f3b0
Vidip Ghosh <vidip2001@gmail.com>
2024-05-29 15:14:56 +0530

    feat: project cards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import GitHubLogin from './GitHubLogin';
+import Cards from './project-section/Cards';
 
 function App() {
   const [projects, setProjects] = useState([])
@@ -17,6 +18,7 @@ function App() {
   return (
     <div>
       <GitHubLogin />
+      <Cards />
       {
         projects.map(({ ID, project_link, project_name, technology_used }) => {
           return (
---
+++ client/src/project-section/Cards.jsx
@@ -0,0 +1,134 @@
+import { useEffect, useState } from "react";
+
+const Cards = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const badgeStyles = [
+    { name: 'ReactJs', style: 'bg-blue-400 text-blue-800' },
+    { name: 'WebGL', style: 'bg-green-400 text-green-800' },
+    { name: 'Threejs', style: 'bg-yellow-400 text-yellow-800' },
+    { name: 'Unity', style: 'bg-red-400 text-red-800' },
+    { name: 'AR/VR', style: 'bg-indigo-400 text-indigo-800' },
+    { name: 'DeepLearning', style: 'bg-blue-400 text-blue-800' },
+    { name: 'NeuralNetworks', style: 'bg-green-400 text-green-800' },
+    { name: 'ImageProcessing', style: 'bg-yellow-400 text-yellow-800' },
+    { name: 'NaturalLanguageProcessing', style: 'bg-red-400 text-red-800' },
+    { name: 'Tensorflow', style: 'bg-indigo-400 text-indigo-800' },
+    { name: 'OpenCV', style: 'bg-purple-400 text-purple-800' },
+    { name: 'Javascript', style: 'bg-blue-400 text-blue-800' },
+    { name: 'Python', style: 'bg-yellow-400 text-yellow-800' },
+    { name: 'Java', style: 'bg-red-400 text-red-800' },
+    { name: 'Data-science', style: 'bg-indigo-400 text-indigo-800' },
+    { name: 'flutter', style: 'bg-purple-400 text-purple-800' },
+    { name: 'HTML', style: 'bg-blue-400 text-blue-800' },
+    { name: 'CSS', style: 'bg-green-400 text-green-800' },
+    { name: 'Bootstrap', style: 'bg-red-400 text-red-800' },
+    { name: 'WebSocket', style: 'bg-indigo-400 text-indigo-800' },
+    { name: 'jQuery', style: 'bg-purple-400 text-purple-800' },
+    { name: 'PHP', style: 'bg-blue-400 text-blue-800' },
+    { name: 'MySQL', style: 'bg-green-400 text-green-800' },
+    { name: 'Pythonframeworrk', style: 'bg-blue-400 text-blue-800' },
+    { name: 'Typescript', style: 'bg-green-400 text-green-800' },
+    { name: 'Tailwind', style: 'bg-yellow-400 text-yellow-800' },
+    { name: 'Kotlin', style: 'bg-red-400 text-red-800' },
+    { name: 'JetpackCompose', style: 'bg-indigo-400 text-indigo-800' },
+    { name: 'Tkinter', style: 'bg-purple-400 text-purple-800' },
+    { name: 'MongoDB', style: 'bg-blue-400 text-blue-800' },
+    { name: 'SQL', style: 'bg-green-400 text-green-800' },
+    { name: 'Flask', style: 'bg-yellow-400 text-yellow-800' },
+    { name: 'Django', style: 'bg-red-400 text-red-800' },
+    { name: 'SCSS', style: 'bg-indigo-400 text-indigo-800' },
+    { name: 'GitHubAPI', style: 'bg-purple-400 text-purple-800' },
+    { name: 'Figma', style: 'bg-blue-400 text-blue-800' },
+    { name: 'NextJS14', style: 'bg-green-400 text-green-800' },
+    { name: 'TailwindCSS', style: 'bg-yellow-400 text-yellow-800' },
+    { name: 'GSAP', style: 'bg-red-400 text-red-800' },
+    { name: 'Framer-motion', style: 'bg-indigo-400 text-indigo-800' },
+    { name: 'NodeJS', style: 'bg-purple-400 text-purple-800' },
+    { name: 'ExpressJS', style: 'bg-blue-400 text-blue-800' },
+    { name: 'GenAI', style: 'bg-green-400 text-green-800' },
+    { name: 'WebSocket', style: 'bg-yellow-400 text-yellow-800' },
+    { name: 'ReactJS', style: 'bg-red-400 text-red-800' },
+    { name: 'Redux', style: 'bg-indigo-400 text-indigo-800' },
+    { name: 'MaterialUI', style: 'bg-purple-400 text-purple-800' },
+    { name: 'MERNSTACK(Mongodb,Express.js,React.js,Node.js)', style: 'bg-blue-400 text-blue-800' },
+    { name: 'Postgres', style: 'bg-yellow-400 text-yellow-800' },
+    { name: 'Sequelize', style: 'bg-purple-400 text-purple-800' },
+    { name: 'Redux', style: 'bg-green-400 text-green-800' },
+  ];
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://golang-main-symbiosis-uni-00b7344c.koyeb.app/projects/all');
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      }
+      const fetchedData = await response.json();
+      const formattedData = fetchedData.map(project => ({
+        ...project,
+        CreatedAt: new Date(project.CreatedAt).toLocaleString(),
+        UpdatedAt: new Date(project.UpdatedAt).toLocaleString(),
+        LastPRMergedAt: new Date(project.LastPRMergedAt).toLocaleString(),
+      }));
+
+      console.log(formattedData);
+      setData(formattedData);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="container px-5 py-24 mx-auto">
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {data.map((project) => (
+            <div key={project.ID} className="border border-amber-600 p-4 rounded-lg shadow-lg h-fit overflow-auto">
+              <h1 className="text-xl font-semibold mb-2">{project.project_name}</h1>
+              <p className="text-gray-600 mb-1">
+                <span className="font-bold">Created At:</span> {project.CreatedAt}
+              </p>
+              <p className="text-gray-600 mb-1">
+                <span className="font-bold">Open Issues:</span> {project.OpenIssueCount}
+              </p>
+              <p className="text-gray-600 mb-1">
+                <span className="font-bold">Last PR Merged At:</span> {project.LastPRMergedAt}
+              </p>
+              <p className="text-gray-600 mb-1">
+                <span className="font-bold">Open PR Count:</span> {project.OpenPRCount}
+              </p>
+              <p className="text-gray-600 mb-4">
+                <span className="font-bold">Updated At:</span> {project.UpdatedAt}
+              </p>
+              <p>
+                {badgeStyles.map((badge, index) => {
+                    if (project.technology_used.includes(badge.name)) {
+                    return (
+                        <span
+                        key={`${badge.name}-${index}`}
+                        className={`text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:text-gray-600 ${badge.style}`}
+                        >
+                        {badge.name}
+                        </span>
+                    );
+                    }
+                    return null;
+                })}
+              </p>
+              <a href={project.project_link}><button type="button" className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Link to the project</button></a>
+          </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cards;
