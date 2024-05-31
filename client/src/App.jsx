a919b99a02a6c7722ce3f866c6c3533dce51e1d4
Kailash Choudhary <kailashjat604@gmail.com>
2024-05-31 20:24:42 +0530

    add footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,18 @@ import Projects from './components/Projects'
 function App() {
 
   return (
-    <div className='flex p-5 flex-col'>
+    <div className='flex p-5 gap-5 flex-col'>
       <div>
         <GitHubLogin />
       </div>
       <Projects />
+      <footer className="p-4 bg-blue-200 text-blue-900 rounded-md shadow-md text-lg">
+        <div className="container mx-auto text-center">
+          <p className="text-sm">
+            Projects data is copied from the official GSSoC site.
+          </p>
+        </div>
+      </footer>
     </div>
   )
 }
