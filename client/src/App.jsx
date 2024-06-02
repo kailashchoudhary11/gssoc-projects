86ec4c7e3778839d4e9352b83f2741d593cc7500
Vidip Ghosh <vidip2001@gmail.com>
2024-06-02 10:48:04 +0530

    feat: repo stars

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,33 @@
 import './App.css'
 import GitHubLogin from './components/GitHubLogin'
 import Projects from './components/Projects'
+import {VITE_GH_TOKEN,VITE_REPO_OWNER,VITE_REPO_NAME} from './constants/baseUrl'
+import { Octokit } from "@octokit/core";
+import {  useState } from "react";
+import { IoStarSharp } from "react-icons/io5";
 
 function App() {
-
+  const [stars,setStars] = useState(0);
+  const octokit = new Octokit({
+    auth: VITE_GH_TOKEN
+  })
+  const getStars = async()=>{
+    const repoStars = await octokit.request('GET /repos/{owner}/{repo}/stargazers', {
+      owner: VITE_REPO_OWNER,
+      repo: VITE_REPO_NAME,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
+    console.log("Stars: ", repoStars.data.length);
+    setStars(repoStars.data.length);
+  }
+  getStars();
   return (
     <div className='flex p-5 gap-5 flex-col'>
-      <div>
+      <div className='flex'>
         <GitHubLogin />
+        <button type="button" className="flex justify-center items-center text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"> <IoStarSharp/> {" "} {stars}</button>
       </div>
       <Projects />
       <footer className="p-4 bg-blue-200 text-blue-900 rounded-md shadow-md text-lg">
--- client/src/constants/baseUrl.js
+++ client/src/constants/baseUrl.js
@@ -1,4 +1,5 @@
-const BASE_URL = import.meta.env.VITE_BASE_URL
-export {
-	BASE_URL
-}
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const VITE_GH_TOKEN = import.meta.env.VITE_GH_TOKEN;
+const VITE_REPO_OWNER = import.meta.env.VITE_REPO_OWNER;
+const VITE_REPO_NAME = import.meta.env.VITE_REPO_NAME;
+export { BASE_URL, VITE_GH_TOKEN, VITE_REPO_OWNER, VITE_REPO_NAME };
